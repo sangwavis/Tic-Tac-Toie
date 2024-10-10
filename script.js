1c2b4701ce581bcb7224df18b6cbff1cac446962
sangwavis <sangwavishvendra@gmail.com>
2024-10-10 15:18:20 +0530

    Create script.js
    main js dir

---
+++ script.js
@@ -0,0 +1,41 @@
+const statusDisplay = document.querySelector('.game--status');
+
+let gameActive = true;
+let currentPlayer = "X";
+let gameState = ["", "", "", "", "", "", "", "", ""];
+
+const winningMessage = () =&gt; `Player ${currentPlayer} has won!`;
+const drawMessage = () =&gt; `Game ended in a draw!`;
+const currentPlayerTurn = () =&gt; `It's ${currentPlayer}'s turn`;
+
+statusDisplay.innerHTML = currentPlayerTurn();
+
+const winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+function handleCellPlayed(clickedCell, clickedCellIndex) {
+    gameState[clickedCellIndex] = currentPlayer;
+    clickedCell.innerHTML = currentPlayer;
+}
+
+function handlePlayerChange() {
+    currentPlayer = currentPlayer === "X" ? "O" : "X";
+    statusDisplay.innerHTML = currentPlayerTurn();
+}
+
+function handleResultValidation() {
+    let roundWon = false;
+    for (let i = 0; i  cell.innerHTML = "");
+}
+
+
+document.querySelectorAll('.cell').forEach(cell =&gt; cell.addEventListener('click', handleCellClick));
+document.querySelector('.game--restart').addEventListener('click', handleRestartGame);
